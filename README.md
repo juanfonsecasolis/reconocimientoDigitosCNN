a8f881390edf2a14f8f98da4a0da9fef4b9dd735
Juan Fonseca <juan.fonsecasolis@outlook.com>
2025-07-22 16:34:12 -0600

    Added status badge to readme, code to write confusion matrix to file, and to upload artifact in pipeline.

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # reconocimientoDigitosCNN
 2021 - Luis Jimenez, Juan M. Fonseca (last updated on July 2025).
 
+[![Python Package using Conda](https://github.com/juanfonsecasolis/reconocimientoDigitosCNN/actions/workflows/python-package-conda.yml/badge.svg)](https://github.com/juanfonsecasolis/reconocimientoDigitosCNN/actions/workflows/python-package-conda.yml)
+
 Code to replicate the results published on the paper [Reconocimiento de dígitos escritos a mano usando aprendizaje profundo](https://www.academia.edu/40196440/Reconocimiento_de_d%C3%ADgitos_escritos_a_mano_usando_aprendizaje_profundo) of the course MP6127.
 
 ## Set up
--- src/proyecto3_confusionMatrix.py
+++ src/proyecto3_confusionMatrix.py
@@ -6,7 +6,7 @@
 # Referencias:
 # - https://stats.stackexchange.com/questions/51296/how-do-you-calculate-precision-and-recall-for-multiclass-classification-using-co
 
-from __future__ import print_function
+from __future__ import log_function
 from proyecto3_evaluate import *
 from proyecto3_utils import *
 from proyecto3_train import get_formated_mnist_data
@@ -15,32 +15,34 @@ import keras
 NUM_DIGITS_MNIST = 10
 confusion_matrix = np.zeros((NUM_DIGITS_MNIST,NUM_DIGITS_MNIST)) 
 
+def log(text):
+	with open(DEFAULT_METRICS_FILEPATH, 'rw') as file:
+		file.write(text + '\n')
+
 def compute_confusion_matrix(model: keras.Sequential, X_test, Y_test):
 	
 	N = len(X_test)
 	for i in range(N):
 		y_true, x = Y_test[i], X_test[i]
-		# print('X shape: ' + str(x.shape))
-		# print('Model input shape: ' + str(model.input_shape))
 		y_pred = model.predict(x.reshape(-1, IMG_ROWS, IMG_COLS, NUM_CHANNELS))
 		i = int(np.argmax(y_pred))
 		j = int(np.argmax(y_true))
 		confusion_matrix[i][j] += 1
 
-	# print confusion matrix
-	print('\nConfusion Matrix',end='\n\t')
+	# log confusion matrix
+	log('\nConfusion Matrix',end='\n\t')
 	for i in range(NUM_DIGITS_MNIST):
-		print(str(i),end='\t')
-	print('')
+		log(str(i),end='\t')
+	log('')
 	for i in range(NUM_DIGITS_MNIST):
-		print(str(i)+':',end='\t')
+		log(str(i)+':',end='\t')
 		for j in range(NUM_DIGITS_MNIST):
-			print(confusion_matrix[i][j], end='\t')
-		print('')
-	print('\nTotal samples analyzed: ' + str(np.sum(confusion_matrix)),end='\n')
+			log(confusion_matrix[i][j], end='\t')
+		log('')
+	log('\nTotal samples analyzed: ' + str(np.sum(confusion_matrix)),end='\n')
 
-def compute_and_print_metrics():
-	print('\nMetrics per category:')
+def compute_and_log_metrics():
+	log('\nMetrics per category:')
 	TPS = []
 	FPS = []
 	FNS = []
@@ -54,7 +56,7 @@ def compute_and_print_metrics():
 	max_precision = 0
 
 	for i in range(NUM_DIGITS_MNIST): 	
-		print('* Category '+str(i)+': ',end='')
+		log('* Category '+str(i)+': ',end='')
 		TP = confusion_matrix[i][i] # TP = A[0,0] = C[i,i] 
 		FP = 0
 		FN = 0
@@ -80,11 +82,11 @@ def compute_and_print_metrics():
 			for j in J:
 				TN += confusion_matrix[j,k] # TN = A[1,1] = sum_{k!=i}{ sum_{j!=i}{ C[j,k] } }	
 
-		print('TP=' + str(TP) + ', ', end='')
-		print('FP=' + str(FP) + ', ', end='')
-		print('FN=' + str(FN) + ', ', end='')
-		print('TN=' + str(TN) + ', ', end='\n')
-		[sensitivity, specificity, precision] = calculate_and_print_advanced_metrics(TP,FP,TN,FN)
+		log('TP=' + str(TP) + ', ', end='')
+		log('FP=' + str(FP) + ', ', end='')
+		log('FN=' + str(FN) + ', ', end='')
+		log('TN=' + str(TN) + ', ', end='\n')
+		[sensitivity, specificity, precision] = calculate_and_log_advanced_metrics(TP,FP,TN,FN)
 		TPS.append(TP)
 		FPS.append(FP)
 		FNS.append(FN)
@@ -102,30 +104,30 @@ def compute_and_print_metrics():
 	mTN = np.mean(TNS)
 	mFN = np.mean(FNS)
 
-	print('\nAverage:')
-	print('TP=' + str(mTP) + ', ', end='')
-	print('FP=' + str(mFP) + ', ', end='')
-	print('TN=' + str(mTN) + ', ', end='')
-	print('FN=' + str(mFN) + ', ', end='')
-	print('')
-	calculate_and_print_advanced_metrics(mTP, mFP, mTN, mFN)
+	log('\nAverage:')
+	log('TP=' + str(mTP) + ', ', end='')
+	log('FP=' + str(mFP) + ', ', end='')
+	log('TN=' + str(mTN) + ', ', end='')
+	log('FN=' + str(mFN) + ', ', end='')
+	log('')
+	calculate_and_log_advanced_metrics(mTP, mFP, mTN, mFN)
 
-	print('Min/max')
-	print('Min sensitivity = ' + str(min_sensitivity))
-	print('Min specificity = ' + str(min_specificity))
-	print('Min precision = ' + str(min_precision))
-	print('Max sensitivity = ' + str(max_sensitivity))
-	print('Max specificity = ' + str(max_specificity))
-	print('Max precision = ' + str(max_precision))
+	log('Min/max')
+	log('Min sensitivity = ' + str(min_sensitivity))
+	log('Min specificity = ' + str(min_specificity))
+	log('Min precision = ' + str(min_precision))
+	log('Max sensitivity = ' + str(max_sensitivity))
+	log('Max specificity = ' + str(max_specificity))
+	log('Max precision = ' + str(max_precision))
 
-def calculate_and_print_advanced_metrics(TP,FP,TN,FN):
+def calculate_and_log_advanced_metrics(TP,FP,TN,FN):
 	sensitivity = TP/(TP+FN)
 	specificity = TN/(TN+FP)
 	precision = TP/(TP+FP)
-	print('Sensitivity = '+str(sensitivity))
-	print('Specificity = '+str(specificity))
-	print('Precision = '+str(precision))
-	print('')
+	log('Sensitivity = '+str(sensitivity))
+	log('Specificity = '+str(specificity))
+	log('Precision = '+str(precision))
+	log('')
 	return sensitivity, specificity, precision
 
 def parse_args():
@@ -150,4 +152,4 @@ if '__main__' == __name__:
 
 	# compute the confusion matrix and associated metrics
 	compute_confusion_matrix(model, X_test, Y_test)
-	compute_and_print_metrics()
+	compute_and_log_metrics()
